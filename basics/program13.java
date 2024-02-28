9aa5af240d88cc71ea45c82b62fd60eae82bb783
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-02-28 07:25:14 +0530

    fc

--- basics/program13.java
+++ basics/program13.java
@@ -1,9 +1,12 @@
+package basics;
+
 import java.util.Random;
 
-class program32 {
+public class program13 {
+
     public static void main(String[] args) {
         Random rand = new Random();
-        int freq[] = new int[6];
+        int[] freq = new int[6];
         for (int i = 0; i < 100; i++)
             ++freq[rand.nextInt(6)];
         for (int i = 0; i < 6; i++)

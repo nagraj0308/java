6182925639fe48285171abf8f30c45e886b3e968
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-02-28 11:26:46 +0530

    fc

--- swing/program04.java
+++ swing/program04.java
@@ -1,17 +1,15 @@
-import java.awt.FlowLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JFrame;
-import javax.swing.Icon;
-import javax.swing.JButton;
-import javax.swing.ImageIcon;
-import javax.swing.JOptionPane;
+package swing;
 
-class program52a extends JFrame implements ActionListener {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class program04a extends JFrame implements ActionListener {
     private JButton reg;
     private JButton custom;
 
-    program52a() {
+    program04a() {
         super("The Title");
         setLayout(new FlowLayout());
 
@@ -39,9 +37,9 @@ class program52a extends JFrame implements ActionListener {
 
 }
 
-public class program52 {
-    public static void main(String args[]) {
-        program52a o1 = new program52a();
+public class program04 {
+    public static void main(String[] args) {
+        program04a o1 = new program04a();
         o1.setSize(500, 300);
         o1.setVisible(true);
         o1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

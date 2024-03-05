ee318f9ba00b9ce7cdfb86a78f2328d4c3ed7690
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-03-05 07:55:30 +0530

    fc

--- best450/P014MergeIntervals.java
+++ best450/P014MergeIntervals.java
@@ -1,31 +1,72 @@
 package best450;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Stack;
 
 public class P014MergeIntervals {
 
     public static void main(String[] args) {
-        int[][] arr = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        Interval arr[] = new Interval[4];
+        arr[0] = new Interval(6, 8);
+        arr[1] = new Interval(1, 9);
+        arr[2] = new Interval(2, 4);
+        arr[3] = new Interval(4, 7);
+        mergeIntervals(arr);
 
-        ArrayList<int[]> list = new ArrayList<>();
-        int[] a = arr[0];
-        int[] temp = arr[0];
-        for (int[] i : arr) {
-            if (a[1] < i[0]) {
-                list.add(a);
-                a = i;
-                System.out.println(a[0] + ":" + a[1]);
-            } else if (a[1] < i[1]) {
-                temp = i;
-                a[1] = i[1];
-                System.out.println(a[0] + ":" + a[1]);
+    }
+
+    public static void mergeIntervals(Interval[] arr) {
+        // Test if the given set has at least one interval
+        if (arr.length == 0)
+            return;
+
+        // Create an empty stack of intervals
+        Stack<Interval> stack = new Stack<>();
+
+        // sort the intervals in increasing order of start
+        // time
+        Arrays.sort(arr, (i1, i2) -> i1.start - i2.start);
+
+        // push the first interval to stack
+        stack.push(arr[0]);
+
+        // Start from the next interval and merge if
+        // necessary
+        for (int i = 1; i < arr.length; i++) {
+            // get interval from stack top
+            Interval top = stack.peek();
+
+            // if current interval is not overlapping with
+            // stack top, push it to the stack
+            if (top.end < arr[i].start)
+                stack.push(arr[i]);
+
+                // Otherwise update the ending time of top if
+                // ending of current interval is more
+            else if (top.end < arr[i].end) {
+                top.end = arr[i].end;
+                stack.pop();
+                stack.push(top);
             }
-
         }
 
-        for (int[] b : list) {
-            System.out.println(b[0] + "," + b[1]);
+        // Print contents of stack
+        System.out.print("The Merged Intervals are: ");
+        while (!stack.isEmpty()) {
+            Interval t = stack.pop();
+            System.out.print("[" + t.start + "," + t.end
+                    + "] ");
         }
+    }
+}
 
+class Interval {
+    int start, end;
+
+    Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
     }
 }

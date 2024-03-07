ddadff4d476676db9ced060b4d2e15c0f7863e04
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-03-07 07:25:56 +0530

    fc

--- best450/P021SubArraySuZero.java
+++ best450/P021SubArraySuZero.java
@@ -3,25 +3,20 @@ package best450;
 public class P021SubArraySuZero {
 
     public static void main(String[] args) {
-        int[] arr = {4,2,-3,1,6};
-        System.out.println("Maximum contiguous sum is "
-                + maxSubArraySum(arr));
+        int[] arr = {4, 2, -3, 1, 6};
+        System.out.println(maxSubArraySum(arr));
     }
 
     // Function Call
     static boolean maxSubArraySum(int[] arr) {
 
         boolean res = false;
-
-        int k = -1;
-        int l = -1;
-        int ls = 0;
         for (int i = 0; i < arr.length; i++) {
             int sum = 0;
             for (int j = i; j < arr.length; j++) {
                 sum += arr[j];
-                if ( sum==0) {
-                   res=true;
+                if (sum == 0) {
+                    res = true;
                 }
             }
         }
